3246ee288d5dc4cfa9c5ee0aae086bc27497a60e
fvdime <73942727+fvdime@users.noreply.github.com>
2024-02-29 22:20:53 +0300

    add auth

--- src/components/forms/auth-form.tsx
+++ src/components/forms/auth-form.tsx
@@ -1,26 +1,81 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+
 import Link from "next/link";
 import Image from "next/image";
 import Input from "../shared/input";
 import Button from "../shared/button";
 import Logo from "../shared/logo";
+import { loginUser, registerUser } from "@/actions/auth.actions";
 
 interface AuthFormProps {
   type: "Login" | "Register";
 }
 
 export default function AuthForm({ type }: AuthFormProps) {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+  
+    const formData = new FormData();
+    
+    formData.append("email", email);
+    formData.append("password", password);
+    
+    console.log("Form submitted with:", formData);
+    
+    if (type === "Register") {
+      try {
+        formData.append("username", username);
+  
+        const newUser = await registerUser(formData);
+        toast.success("Registered Successfully!");
+  
+        router.push("/feed");
+  
+        console.log("Response: ", newUser);
+      } catch (error) {
+        router.refresh();
+        toast.error("Permission denied!");
+        console.error("Error:", error);
+      }
+    }
+  
+    if (type === "Login") {
+      try {
+        const user = await loginUser(formData);
+        toast.success("Logged In Successfully!");
+  
+        router.push("/feed");
+  
+        console.log("Response: ", user);
+      } catch (error) {
+        router.refresh();
+        toast.error("Permission denied!");
+        console.error("Error:", error);
+      }
+    }
+  };  
+
   return (
     <div className="flex flex-row justify-center items-center h-full w-screen">
       <form
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
         className="h-full w-full md:w-2/5 py-16 px-8 md:px-16  flex flex-col justify-center"
       >
         <div className="flex items-center justify-center mb-5">
           <p className="text-3xl">
-          {type === "Register" ? "Create new account!" : "Log In to your Account!"}
+            {type === "Register"
+              ? "Create new account!"
+              : "Log In to your Account!"}
           </p>
         </div>
         <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300"></div>
@@ -28,11 +83,9 @@ export default function AuthForm({ type }: AuthFormProps) {
           <div className="mb-4">
             <Input
               placeholder="username"
-              // disabled={}
               type="text"
-              // value={values.username}
-              // onChange={handleChange}
-              // onBlur={handleBlur}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               name="username"
               id="username"
             />
@@ -45,9 +98,8 @@ export default function AuthForm({ type }: AuthFormProps) {
             placeholder="email"
             // disabled={}
             type="email"
-            // value={values.username}
-            // onChange={handleChange}
-            // onBlur={handleBlur}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             name="email"
             id="email"
           />
@@ -57,15 +109,18 @@ export default function AuthForm({ type }: AuthFormProps) {
             placeholder="password"
             // disabled={}
             type="password"
-            // value={values.username}
-            // onChange={handleChange}
-            // onBlur={handleBlur}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             name="password"
             id="password"
           />
         </div>
         <div className="text-center">
-          <Button label={type === "Register" ? "Register" : "Log In"} fullWidth large />
+          <Button
+            label={type === "Register" ? "Register" : "Log In"}
+            fullWidth
+            large
+          />
           <p className="mb-0 mt-4 pt-1 text-sm font-medium text-center">
             {type === "Register"
               ? "Already have an account?"
@@ -79,8 +134,8 @@ export default function AuthForm({ type }: AuthFormProps) {
           </p>
         </div>
       </form>
-      <div className="hidden md:flex h-screen w-3/5 bg-secondary p-4"/>
-      {/* <Toaster position="top-right" reverseOrder={false} /> */}
+      <div className="hidden md:flex h-screen w-3/5 bg-secondary p-4" />
+      <Toaster position="top-right" reverseOrder={false} />
     </div>
   );
 }
