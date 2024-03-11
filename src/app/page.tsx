1fbd92aace1626cca3a1d91803b6d26b38395236
fvdime <73942727+fvdime@users.noreply.github.com>
2024-03-11 23:57:10 +0300

    fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import About from "@/components/home/about";
-import Footer from "@/components/home/footer";
 import HeroBanner from "@/components/home/hero-banner";
-import Parallax from "@/components/home/parallax/index";
-import TextSlide from "@/components/home/text-slide";
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
-      {/* <Parallax /> */}
-      {/* <About/>
-      <TextSlide/>
-      <Footer /> */}
     </>
   );
 }
